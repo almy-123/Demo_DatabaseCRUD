943696bd61ff438d85ed24edb06e03290b47f8c8
19037610 <19037610@myrp.edu.sg>
2021-05-18 11:01:31 +0800

    created EditActivity activity

---
+++ app/src/main/java/sg/edu/rp/id19037610/demodatabasecrud/EditActivity.java
@@ -0,0 +1,54 @@
+package sg.edu.rp.id19037610.demodatabasecrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        tvID = findViewById(R.id.tvID);
+        etContent = findViewById(R.id.etContent);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(EditActivity.this);
+                data.setNoteContent(etContent.getText().toString());
+                dbHelper.updateNote(data);
+                dbHelper.close();
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbHelper = new DBHelper(EditActivity.this);
+                dbHelper.deleteNote(data.getId());
+                dbHelper.close();
+            }
+        });
+    }
+}
--- app/src/main/java/sg/edu/rp/id19037610/demodatabasecrud/MainActivity.java
+++ app/src/main/java/sg/edu/rp/id19037610/demodatabasecrud/MainActivity.java
@@ -2,13 +2,80 @@ package sg.edu.rp.id19037610.demodatabasecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnAdd, btnEdit, btnRetrieve;
+    TextView tvDBContent;
+    EditText etContent;
+    ArrayList<Note> al;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        al = new ArrayList<Note>();
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnEdit = findViewById(R.id.btnEdit);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
+        etContent = findViewById(R.id.etContent);
+        tvDBContent = findViewById(R.id.tvDBContent);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = etContent.getText().toString();
+                DBHelper dbHelper = new DBHelper(MainActivity.this);
+                long inserted_id = dbHelper.insertNote(data);
+                dbHelper.close();
+
+                if (inserted_id != -1){
+                    Toast.makeText(MainActivity.this, "Successfully Inserted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(MainActivity.this);
+                al.clear();
+                al.addAll(dbh.getAllNotes());
+                dbh.close();
+
+                String txt = "";
+                for (int i = 0; i< al.size(); i++){
+                    Note tmp = al.get(i);
+                    txt += "ID:" + tmp.getId() + ", " +
+                            tmp.getNoteContent() + "\n";
+                }
+                tvDBContent.setText(txt);
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Note target = al.get(0);
+
+                Intent i = new Intent(MainActivity.this, EditActivity.class);
+                i.putExtra("data", target);
+                startActivity(i);
+            }
+        });
+
     }
 }
